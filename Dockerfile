dcd820088a543cd4050b339e0465b8163c9e280f
michael turanov <mturanov@briogroup.ru>
2024-11-29 14:24:36 +0300

    add and debug entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN pip install --upgrade pip
 
 COPY ./requirements.txt .
 
+COPY ./entrypoint.sh /entrypoint.sh
+
 RUN pip install -r requirements.txt
 
 
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Waiting for database..."
+while ! nc -z db 5432; do
+  sleep 1
+done
+
+echo "Database ready!"
+
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
+exec "$@"
